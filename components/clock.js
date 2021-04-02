5198b93a5f15ad320e221ab0348d298946a43ca1
Siim Männart <siim@siim.me>
2021-04-03 07:01:59 +1100

    padding and radius fix

--- components/clock.js
+++ components/clock.js
@@ -5,7 +5,7 @@ function Digit({ digit }) {
       style={{ width: "11.675%" }}
     >
       <div className="absolute flex items-center justify-center w-full h-full text-7xl">
-        {!isNaN(parseInt(digit)) && <p className="pb-3 text-digit" style={{fontSize: "11vw", transform: `scale(1, 1.4`, fontFamily: 'Inconsolata' }}>{digit}</p>}
+        {!isNaN(parseInt(digit)) && <p className="md:pb-1 lg:pb-2 text-digit" style={{fontSize: "11vw", transform: `scale(1, 1.4`, fontFamily: 'Inconsolata' }}>{digit}</p>}
         {isNaN(parseInt(digit)) && digit == "S" && (
           <div className="font-semibold leading-tight text-center text-digit" style={{ fontFamily: 'Inconsolata'}}>
             <p style={{fontSize: "2vw"}}>MOSCOW</p>
@@ -44,7 +44,7 @@ function Digits({ digits = ["0", "0", "0", "0", "0", "0", "0"] }) {
 export default function Clock({ digits }) {
   return (
     <div
-      className="relative w-3/4 overflow-hidden bg-gray-700 bg-contain cursor-default pointer-events-none rounded-4xl pb-clock"
+      className="relative w-3/4 overflow-hidden bg-gray-700 bg-contain rounded-md cursor-default pointer-events-none lg:rounded-2xl xl:rounded-3xl 2xl:rounded-4xl md:rounded-xl sm:rounded-lg pb-clock"
       style={{ backgroundImage: `url('/blockclock.jpg')` }}
     >
       <div className="absolute w-full h-full">
