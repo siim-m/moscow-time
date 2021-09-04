b61fdf45b55c3b46f1d6c740d90851718abe48ba
Siim Männart <siim@siim.me>
2021-09-04 14:43:23 +1000

    Major refactor

--- widget.js
+++ widget.js
@@ -35,17 +35,9 @@ const CLOCK_HTML = `
 </div>
 `;
 
-/**
- * To change the order of different views, change the order of
- * elements in this array.
- */
-const OPTIONS = ["MOSCOWTIME", "HEIGHT", "BTCUSD", "SATUSD"];
+const OPTIONS = ["HEIGHT", "MOSCOWTIME", "BTCUSD", "SATUSD"];
 
-/**
- * To change how often the views change, set CYCLE_INTERVAL to an appropriate
- * value in milliseconds.
- */
-const CYCLE_INTERVAL = 300000;
+const FETCH_INTERVAL = 10000;
 
 const BTC_USD_HTML = `
   <div>
@@ -71,14 +63,23 @@ const MOSCOW_TIME_HTML = `
   </div>
 `;
 
+const VALID_DISPLAY_OPTIONS = ["blockheight", "usdprice", "satsperdollar", "moscowtime"]
+
+let displayOptions;
+let activeOption;
+let activeView;
+let cycleInterval = 3000;
+
+let priceUsd;
+let blockHeight;
+let satUsd;
+
 // Sets the values of the top section of each cell
 function setTopSections(values) {
   for (let i = 0; i < 7; i += 1) {
     document.getElementById(`top-section-${i}`).innerHTML = values[i] || "";
   }
 }
-
-let activeView = OPTIONS[0];
 
 function setDimensions() {
   const clockContainer = document.getElementById("blockclock-container");
@@ -87,7 +88,7 @@ function setDimensions() {
 
   clockContainer.style.height = `${0.336 * clockContainer.clientWidth}px`;
   clock.style.height = `${0.336 * clockContainer.clientWidth}px`;
-  digits.style.height = `${0.59 * clock.clientHeight}px`
+  digits.style.height = `${0.59 * clock.clientHeight}px`;
 
   Array.from(document.getElementsByClassName("digit-content")).forEach((el) => {
     el.style.fontSize = `${0.49 * clock.clientHeight}px`;
@@ -110,180 +111,90 @@ function setDimensions() {
   );
 }
 
-// All main logic lives in this function
 function cycleView() {
-  switch (activeView) {
-    case OPTIONS[0]:
-      activeView = OPTIONS[1];
-      break;
-    case OPTIONS[1]:
-      activeView = OPTIONS[2];
-      break;
-    case OPTIONS[2]:
-      activeView = OPTIONS[3];
-      break;
-    case OPTIONS[3]:
-      activeView = OPTIONS[0];
-      break;
-    default:
-      activeView = OPTIONS[0];
-      break;
+  if (activeOption === undefined || activeOption === displayOptions.length - 1) {
+    activeOption = 0;
+  } else {
+    activeOption += 1;
   }
 
+  activeView = displayOptions[activeOption];
+
   switch (activeView) {
-    case "BTCUSD":
-      fetch(
-        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
-      ).then((res) => {
-        res.json().then((data) => {
-          /**
-           * To test longer/shorter price values, comment out the next
-           * line and set btcUsd to an arbitrary number in string format.
-           */
-          let btcUsd = Math.round(parseFloat(data.bitcoin.usd)).toString();
+    case "usdprice":
+      setTopSections(["Market price of", "bitcoin"]);
 
-          // Ensure the first cell has the right CSS class, so it can display special text.
-          // document.getElementById("0").classList.remove("digit-content");
-          // document.getElementById("0").classList.add("special-content");
+      document.getElementById("0").innerHTML = BTC_USD_HTML;
+      document.getElementById("1").innerHTML = "$";
 
-          // Set the top sections.
-          setTopSections(["Market price of", "bitcoin"]);
-
-          // Set first and second cell to show BTC/USD and $ sign.
-          document.getElementById("0").innerHTML = BTC_USD_HTML;
-          document.getElementById("1").innerHTML = "$";
-
-          // Determine which format to use based on number of digits in price.
-          switch (true) {
-            case btcUsd.length > 6:
-              document.getElementById("2").innerHTML = `${btcUsd[0]}.`;
-              document.getElementById("3").innerHTML = btcUsd[1];
-              document.getElementById("4").innerHTML = btcUsd[2];
-              document.getElementById("5").innerHTML = btcUsd[3];
-              document.getElementById("6").innerHTML = "M";
-              break;
-            case btcUsd.length > 5:
-              document.getElementById("2").innerHTML = btcUsd[0];
-              document.getElementById("3").innerHTML = btcUsd[1];
-              document.getElementById("4").innerHTML = `${btcUsd[2]}.`;
-              document.getElementById("5").innerHTML = btcUsd[3];
-              document.getElementById("6").innerHTML = "K";
-              break;
-            default:
-              for (let i = 2; i < 7; i += 1) {
-                document.getElementById(i.toString()).innerHTML =
-                  btcUsd[btcUsd.length - 7 + i] || "";
-              }
-              break;
+      // Determine which format to use based on number of digits in price.
+      switch (true) {
+        case priceUsd.length > 6:
+          document.getElementById("2").innerHTML = `${priceUsd[0]}.`;
+          document.getElementById("3").innerHTML = priceUsd[1];
+          document.getElementById("4").innerHTML = priceUsd[2];
+          document.getElementById("5").innerHTML = priceUsd[3];
+          document.getElementById("6").innerHTML = "M";
+          break;
+        case priceUsd.length > 5:
+          document.getElementById("2").innerHTML = priceUsd[0];
+          document.getElementById("3").innerHTML = priceUsd[1];
+          document.getElementById("4").innerHTML = `${priceUsd[2]}.`;
+          document.getElementById("5").innerHTML = priceUsd[3];
+          document.getElementById("6").innerHTML = "K";
+          break;
+        default:
+          for (let i = 2; i < 7; i += 1) {
+            document.getElementById(i.toString()).innerHTML =
+              priceUsd[priceUsd.length - 7 + i] || "";
           }
-        });
+          break;
+      }
 
-        // Update dimensions
-        setDimensions();
-      });
+      setDimensions();
+
       break;
 
-    case "SATUSD":
-      fetch(
-        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
-      ).then((res) => {
-        res.json().then((data) => {
-          let btcUsd = parseFloat(data.bitcoin.usd);
+    case "satsperdollar":
+      document.getElementById("0").innerHTML = SAT_USD_HTML;
 
-          /**
-           * To test longer/shorter sats per dollar values, comment out the
-           * next line and set satUsd to an arbitrary number in string format.
-           */
-          let satUsd = Math.round((1 / btcUsd) * 100000000).toString();
+      setTopSections([]);
 
-          // Ensure the first cell has the right CSS class, so it can display special text.
-          // document.getElementById("0").classList.remove("digit-content");
-          // document.getElementById("0").classList.add("special-content");
+      // Set the digits.
+      for (let i = 1; i < 7; i += 1) {
+        document.getElementById(i.toString()).innerHTML =
+          satUsd[satUsd.length - 7 + i] || "";
+      }
 
-          // Set the first cell to show SATS / USD.
-          document.getElementById("0").innerHTML = SAT_USD_HTML;
+      setDimensions();
 
-          /**
-           * If the sats per dollar view should display any text in the upper
-           * sections of the slots, this can be set here. See BTCUSD and HEIGHT
-           * views for examples above and below.
-           */
-          setTopSections([]);
-
-          // Set the digits.
-          for (let i = 1; i < 7; i += 1) {
-            document.getElementById(i.toString()).innerHTML =
-              satUsd[satUsd.length - 7 + i] || "";
-          }
-
-          // Update dimensions
-          setDimensions();
-        });
-      });
       break;
 
-    case "HEIGHT":
-      fetch("https://blockstream.info/api/blocks/tip/height").then((res) => {
-        res.json().then((data) => {
-          /**
-           * To test longer/shorter block height values, comment out the
-           * next line and set blockHeight to an arbitrary number in string format.
-           */
-          let blockHeight = data.toString();
+    case "blockheight":
+      for (let i = 0; i < 7; i += 1) {
+        document.getElementById(i.toString()).innerHTML =
+          blockHeight[blockHeight.length - 7 + i] || "";
+      }
 
-          // Ensure the first cell has the right CSS class, so it can display a digit.
-          // document.getElementById("0").classList.remove("special-content");
-          // document.getElementById("0").classList.add("digit-content");
-
-          for (let i = 0; i < 7; i += 1) {
-            document.getElementById(i.toString()).innerHTML =
-              blockHeight[blockHeight.length - 7 + i] || "";
-          }
-
-          setTopSections(["Number of blocks", "in the", "blockchain"]);
-          // Update dimensions
-          setDimensions();
-        });
-      });
+      setTopSections(["Number of blocks", "in the", "blockchain"]);
+      
+      setDimensions();
+      
       break;
 
-    case "MOSCOWTIME":
-      fetch(
-        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
-      ).then((res) => {
-        res.json().then((data) => {
-          let btcUsd = parseFloat(data.bitcoin.usd);
+    case "moscowtime":
+      document.getElementById("0").innerHTML = MOSCOW_TIME_HTML;
 
-          /**
-           * To test longer/shorter sats per dollar values, comment out the
-           * next line and set satUsd to an arbitrary number in string format.
-           */
-          let satUsd = Math.round((1 / btcUsd) * 100000000).toString();
+      setTopSections([]);
 
-          // Ensure the first cell has the right CSS class, so it can display special text.
-          // document.getElementById("0").classList.remove("digit-content");
-          // document.getElementById("0").classList.add("special-content");
+      // Set the digits.
+      for (let i = 1; i < 7; i += 1) {
+        document.getElementById(i.toString()).innerHTML =
+          satUsd[satUsd.length - 7 + i] || "";
+      }
 
-          // Set the first cell to show SATS / USD.
-          document.getElementById("0").innerHTML = MOSCOW_TIME_HTML;
+      setDimensions();
 
-          /**
-           * If the sats per dollar view should display any text in the upper
-           * sections of the slots, this can be set here. See BTCUSD and HEIGHT
-           * views for examples above and below.
-           */
-          setTopSections([]);
-
-          // Set the digits.
-          for (let i = 1; i < 7; i += 1) {
-            document.getElementById(i.toString()).innerHTML =
-              satUsd[satUsd.length - 7 + i] || "";
-          }
-
-          // Update dimensions
-          setDimensions();
-        });
-      });
       break;
 
     default:
@@ -291,22 +202,60 @@ function cycleView() {
   }
 }
 
-const file = location.pathname.split("/").pop();
+async function fetchData() {
+  let priceUsdResponse = await (
+    await fetch(
+      "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+    )
+  ).json();
+
+  priceUsd = priceUsdResponse.bitcoin.usd.toString();
+  satUsd = Math.round((1 / priceUsd) * 100000000).toString();
+
+  let blockHeightResponse = await (
+    await fetch("https://blockstream.info/api/blocks/tip/height")
+  ).json();
+
+  blockHeight = blockHeightResponse.toString();
+}
+
+setInterval(fetchData, FETCH_INTERVAL);
 
 window.onload = () => {
-
+  // Load CSS
   const link = document.createElement("link");
-  link.href = "widget.css"; //file.substr( 0, file.lastIndexOf( "." ) ) + ".css";
+  link.href = "widget.css";
   link.type = "text/css";
   link.rel = "stylesheet";
   document.getElementsByTagName("head")[0].appendChild(link);
-  
-  document.getElementById("blockclock-container").innerHTML = CLOCK_HTML;
-  
+
+  const clockContainer = document.getElementById("blockclock-container")
+
+  const displayClasses = Array.from(clockContainer.classList)
+
+  displayOptions = displayClasses.filter((className) => {
+    return VALID_DISPLAY_OPTIONS.includes(className);
+  });
+
+  displayClasses.forEach((className) => {
+    if (className.includes("interval-")) {
+
+      const regexp = /^(interval-)(\d+)$/gi
+      const matches = [...className.matchAll(regexp)]
+      if (matches.length) {
+        const intervalValue = parseInt(matches[0][2])
+        if (typeof intervalValue === "number") cycleInterval = intervalValue
+      }
+    }
+  })
+
+  clockContainer.innerHTML = CLOCK_HTML;
+
   setDimensions();
   window.onresize = setDimensions;
-  
-  cycleView();
-  setInterval(cycleView, CYCLE_INTERVAL);
-}
 
+  fetchData().then(() => {
+    cycleView();
+    setInterval(cycleView, cycleInterval);
+  });
+};
