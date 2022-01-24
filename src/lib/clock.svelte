eb609cc370eff0bf8be53d4648afeff15e3e2337
Siim <46551195+siim-m@users.noreply.github.com>
2022-01-25 08:29:40 +1100

    add original widget script for testing

---
+++ src/lib/clock.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<script async defer src="widget-with-frame.js"></script>
+</svelte:head>
+
+<div class="bg-blue-300 max-w-2xl mx-auto">
+	<div id="blockclock-container" class="moscowtime usdprice blockheight interval-3000" />
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,15 @@
-<svelte:head>
-  <script src="https://moscowtime.xyz/widget-with-frame.js"></script>
-</svelte:head>
+<script>
+	import Clock from '$lib/clock.svelte';
+</script>
 
-<div class="bg-blue-300 max-w-2xl mx-auto">
- <div id="blockclock-container" class="moscowtime usdprice blockheight interval-3000" />
-</div>
+<Clock />
 
 <footer>
- <div class="prose text-center mx-auto">
-  <p>
-   Check out our <a href="/widget">JavaScript widget</a> · BLOCKCLOCK is a registered
-   trademark of
-   <a href="https://www.coinkite.com" target="_blank" rel="noopener">Coinkite Inc</a>
-  </p>
- </div>
+	<div class="prose text-center mx-auto">
+		<p>
+			Check out our <a href="/widget" rel="external">JavaScript widget</a> · BLOCKCLOCK is a
+			registered trademark of
+			<a href="https://www.coinkite.com" target="_blank" rel="external noopener">Coinkite Inc</a>
+		</p>
+	</div>
 </footer>
