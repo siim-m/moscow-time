011820612b21abd9f2079917f0be014612a9f8e2
Siim <46551195+siim-m@users.noreply.github.com>
2022-02-03 16:38:25 +1100

    remove unused code

--- src/lib/header.svelte
+++
@@ -1,7 +0,0 @@
-<header>
-	<nav>
-		<a href="/">Home</a>
-		<a href="/about">About Us</a>
-		<a href="/posts">Posts</a>
-	</nav>
-</header>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,20 +1,10 @@
 <script>
-	// '$lib' is an alias to the src/lib folder created by Svelte.
-	// jsconfig.json is for VS Code to know about this mapping.
-	// import Header from '$lib/header.svelte';
-
 	import Footer from '$lib/footer.svelte';
 	import '../tailwind.css';
 </script>
 
-<!-- <Header /> -->
-
 <div class="mx-2 sm:mx-6 md:mx-10 lg:mx-14 flex flex-col h-full">
 	<main class="flex-grow">
-		<!--
-    <slot /> is where "children" get mounted in the component.
-    __layout.svelte is a special filename.
-  -->
 		<slot />
 	</main>
 
