fbf610456b2d78eb5f6e71505713d0d5a380f0d2
Siim <46551195+siim-m@users.noreply.github.com>
2022-05-15 20:06:29 +1000

    refactor bot background worker

--- twitter_bot/background.js
+++ twitter_bot/background.js
@@ -5,83 +5,96 @@ const USERS_TO_FOLLOW = [
 	'56562803', // @PeterSchiff
 ];
 
-client.v1
-	.filterStream({
+async function main() {
+	const stream = await client.v1.filterStream({
 		follow: USERS_TO_FOLLOW,
 		tweet_mode: 'extended',
-	})
-	.then((stream) => {
-		stream.on(ETwitterStreamEvent.Connected, () => console.log('Stream connected'));
-		stream.on(ETwitterStreamEvent.Reconnected, () => console.log('Stream reconnected'));
-		stream.on(ETwitterStreamEvent.ConnectionLost, (err) =>
-			console.log('Stream connection lost!', err)
-		);
-		stream.on(ETwitterStreamEvent.ConnectionError, (err) =>
-			console.log('Stream connection error!', err)
-		);
+	});
 
-		stream.on(ETwitterStreamEvent.ConnectionClosed, () =>
-			console.log('Connection has been closed.')
-		);
+	stream.autoReconnect = true;
 
-		// stream.on(ETwitterStreamEvent.DataKeepAlive, () => console.log('Received stream keep-alive'));
+	stream.on(ETwitterStreamEvent.Reconnected, () => console.log(new Date(), 'Stream reconnected!'));
 
-		stream.on(ETwitterStreamEvent.Data, async (eventData) => {
-			if (
-				!eventData.retweeted_status &&
-				USERS_TO_FOLLOW.includes(eventData.user?.id_str) &&
-				(eventData.text.toLowerCase().includes('bitcoin') ||
-					eventData.extended_tweet?.full_text?.toLowerCase().includes('bitcoin'))
-			) {
+	stream.on(ETwitterStreamEvent.ConnectionLost, (err) =>
+		console.error(new Date(), 'Stream connection lost!\n', err)
+	);
+
+	stream.on(
+		// Emitted when Node.js {response} emits a 'error' event (contains its payload).
+		ETwitterStreamEvent.Error,
+		(err) => console.error(new Date(), 'Connection or parse error!\n', err)
+	);
+
+	stream.on(
+		// Emitted when Node.js {response} is closed by remote or using .close().
+		ETwitterStreamEvent.ConnectionClosed,
+		() => console.log(new Date(), 'Connection has been closed.')
+	);
+
+	stream.on(
+		// Emitted when a Twitter sent a signal to maintain connection active
+		ETwitterStreamEvent.DataKeepAlive,
+		() => console.log(new Date(), 'Twitter has a keep-alive packet.')
+	);
+
+	stream.on(ETwitterStreamEvent.Data, async (eventData) => {
+		if (
+			!eventData.retweeted_status &&
+			USERS_TO_FOLLOW.includes(eventData.user?.id_str) &&
+			(eventData.text.toLowerCase().includes('bitcoin') ||
+				eventData.extended_tweet?.full_text?.toLowerCase().includes('bitcoin'))
+		) {
+			console.log(
+				new Date(),
+				'Received tweet matching filter:',
+				`https://twitter.com/${eventData.user.screen_name}/status/${eventData.id_str}`
+			);
+
+			try {
+				const price = await getPrice();
+
+				const screenshot = await getScreenshot({
+					view: 'usdprice',
+					value: price,
+					timestamp: Date.now() / 1000,
+				});
+
+				const text = `🚨 @${
+					eventData.user.screen_name
+				} just tweeted about #bitcoin. The current price is $${price.toLocaleString()}.\n\nIf the past is any indicator of the future, it might be a good time to buy.`;
+
+				const quoteTweet = await client.v1.tweet(text, {
+					media_ids: [
+						await client.v1.uploadMedia(Buffer.from(screenshot), {
+							mimeType: EUploadMimeType.Png,
+						}),
+					],
+
+					// Attach the original tweet as a quote without showing the URL in the text
+					attachment_url: `https://twitter.com/${eventData.user.screen_name}/status/${eventData.id_str}`,
+				});
+
 				console.log(
-					'Received tweet matching filter:',
-					`https://twitter.com/${eventData.user.screen_name}/status/${eventData.id_str}`
+					new Date(),
+					'Tweeted quote tweet:',
+					`https://twitter.com/moscowtime_xyz/status/${quoteTweet.id_str}`
 				);
 
-				try {
-					const price = await getPrice();
+				const reply = await client.v1.reply(`Let's see how well this ages...`, eventData.id_str, {
+					// Attach the quote tweet as a reply to the original without showing the URL in the text.
+					attachment_url: `https://twitter.com/${quoteTweet.user.screen_name}/status/${quoteTweet.id_str}`,
+				});
 
-					const screenshot = await getScreenshot({
-						view: 'usdprice',
-						value: price,
-						timestamp: Date.now() / 1000,
-					});
-
-					const text = `🚨 @${
-						eventData.user.screen_name
-					} just tweeted about #bitcoin. The current price is $${price.toLocaleString()}.\n\nIf the past is any indicator of the future, it might be a good time to buy.`;
-
-					const quoteTweet = await client.v1.tweet(text, {
-						media_ids: [
-							await client.v1.uploadMedia(Buffer.from(screenshot), {
-								mimeType: EUploadMimeType.Png,
-							}),
-						],
-
-						// Attach the original tweet as a quote without showing the URL in the text
-						attachment_url: `https://twitter.com/${eventData.user.screen_name}/status/${eventData.id_str}`,
-					});
-
-					console.log(
-						'Tweeted quote tweet:',
-						`https://twitter.com/moscowtime_xyz/status/${quoteTweet.id_str}`
-					);
-
-					const reply = await client.v1.reply(`Let's see how well this ages...`, eventData.id_str, {
-						// Attach the quote tweet as a reply to the original without showing the URL in the text.
-						attachment_url: `https://twitter.com/${quoteTweet.user.screen_name}/status/${quoteTweet.id_str}`,
-					});
-
-					console.log(
-						'Replied to quoted tweet:',
-						`https://twitter.com/moscowtime_xyz/status/${reply.id_str}`
-					);
-				} catch (err) {
-					console.log('Caught error:', err);
-				}
+				console.log(
+					new Date(),
+					'Replied to quoted tweet:',
+					`https://twitter.com/moscowtime_xyz/status/${reply.id_str}`
+				);
+			} catch (err) {
+				console.log(new Date(), 'Caught error:', err);
 			}
-		});
-
-		// Enable reconnect feature
-		stream.autoReconnect = true;
+		}
 	});
+}
+
+main();
